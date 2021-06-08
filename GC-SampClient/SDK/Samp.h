9d5d909530d992a26f4fbe0f1fd9e6028aa19abe
Amyr Ahmady <hhm6@yahoo.com>
2021-06-08 21:51:52 +0430

    use patterns for AddChatMessage function, no more hardcoded addresses

--- GC-SampClient/SDK/Samp.h
+++ GC-SampClient/SDK/Samp.h
@@ -3,17 +3,7 @@
 
 namespace SAMP
 {
-	typedef void(__thiscall* fnAddMessageToChat) (void* object, uint32_t messageType, const char* text, const char* prefix, uint32_t textColor, uint32_t prefixColor);
-
-	constexpr uintptr_t FN_SEND_MESSAGE_OFFSET = 0x57F0U;
-	constexpr uintptr_t FN_ADD_TO_CHAT_OFFSET = 0x64010U;
-	constexpr uintptr_t ST_CHAT_INFO_OFFSET = 0x21A0E4U;
-
-	constexpr size_t ADD_TO_CHAT_CHUNK_SIZE = 86;
-
 	inline void* base;
-	inline void* chatInfo;
-	inline fnAddMessageToChat AddMessageToChatOriginal;
 
 	bool Initialize();
 	bool AddMessageToChat(const char* message, size_t color = 0x00FF00U);
